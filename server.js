c13f01ebc541e10e09072420212c77137a7a9bc0
Rovak0 <ericjackwong@outlook.com>
2024-07-31 15:13:47 -0400

    start up

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = { 
+  cookie : {
+        maxAge: 60 * 15 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+  },
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.use(session(sess));
+
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on' + PORT));
+});
